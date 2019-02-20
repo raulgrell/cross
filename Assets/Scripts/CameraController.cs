f06b19f0e333d648e239210dc9f9675d9ded02ce
Days99 <diogo.g.dias09@gmail.com>
2019-02-20 18:27:59 +0000

    Camera Work

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject player;
+    private Vector3 bossVector;
+    private Vector3 offset;
+    private Camera camera;
+    float initialpositiony;
+
+    public float smoothSpeed = 0.125f;
+
+    void Start()
+    { 
+        offset = transform.position - player.transform.position;
+        initialpositiony = transform.position.y;
+    }
+
+    void LateUpdate()
+    {
+        if (player != null)
+        {
+                Vector3 desiredPosition = player.transform.position + offset;
+                transform.position = desiredPosition;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rb;
+    private int state;
+
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void setState(int newState)
+    {
+        state = newState;
+    }
+
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+ //       switch (state)
+ //       {
+ //           case 0:
+
+                rb.velocity = new Vector3(moveHorizontal * speed, 0, moveVertical * speed);
+           //     if (rb.velocity.x != 0 && idle)
+           //     {
+                  //  anim.SetBool("walking", true);
+                  //  if (rb.velocity.x > 0)
+                  //  {
+                  //      spriteRenderer.flipX = false;
+                  //  }
+                  //  else
+                   //     spriteRenderer.flipX = true;
+
+         //           idle = false;
+
+
+         //       }
+           //     else if (!idle)
+           //     {
+           //         anim.SetBool("walking", false);
+           //         idle = true;
+           //     }
+  //              break;
+//        }
+        //   if (damaged)
+        //   {
+        //       float time = Time.deltaTime + 0.1f;
+        //       if(time > Time.deltaTime + 5)
+        //       {
+        //           damaged = false;
+        //       }
+
+        //   }
+
+    }
+
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.CompareTag("Ground"))
+    //    {
+    //        Vector3 hit = collision.contacts[0].normal;
+    //        //if(hit)
+    //        if (hit.y == 1)
+    //        {
+    //            onGround = true;
+    //            jumped = false;
+    //      //      state = 0;
+    //        }
+    //    }
+    //    else if (collision.gameObject.CompareTag("Enemy") && !damaged)
+    //    {
+    //    //    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+    //        //   health--;
+    //        //   damaged = true;
+    //    }
+    //}
+
+    //private void OnCollisionExit2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.CompareTag("Ground"))
+    //    {
+    //        onGround = false;
+    //  //      state = 1;
+    //    }
+    //}
+}
