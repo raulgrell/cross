6ea820ec4e576e65b5750bd5ca29ac02d2d3e38a
raulgrell <raulgrell@gmail.com>
2019-02-27 18:03:22 +0000

    Grid stuff, test assets

--- Assets/Scripts/Grid/GridLayer.cs
+++ Assets/Scripts/Grid/GridLayer.cs
@@ -4,13 +4,40 @@ using UnityEngine;
 
 public class GridLayer : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject nodePrefab;
+    public int numCols = 8;
+    public int numRows = 8;
+    public float colWidth = 2;
+    public float colHeight = 2;
+    public float gutterSize = 0.1f;
+    
     void Start()
     {
-        
+        for (int i = 0; i < numCols; i++)
+        {
+            for (int j = 0; j < numRows; j++)
+            {
+                var offset = Vector3.forward * colHeight * j + Vector3.right * colWidth * i;
+                Instantiate(nodePrefab, transform.position + offset, Quaternion.identity, transform);
+            }
+        }
     }
 
-    // Update is called once per frame
+    private void OnDrawGizmos()
+    {
+        Gizmos.matrix = transform.localToWorldMatrix;
+
+        var gutterOffset = new Vector3(gutterSize, 0, gutterSize);
+        for (int i = 0; i < numCols; i++)
+        {
+            for (int j = 0; j < numRows; j++)
+            {
+                var offset = Vector3.forward * colHeight * j + Vector3.right * colWidth * i + gutterOffset;
+                Gizmos.DrawWireCube(offset, new Vector3(2, 0.1f, 2) - gutterOffset);
+            }
+        }
+    }
+
     void Update()
     {
         
