e158ed03fec683653e5891cff4f07148f10c3a1d
Days99 <diogo.g.dias09@gmail.com>
2019-03-10 02:53:20 +0000

    Basic tweening using step

--- Assets/Scripts/CutsceneSystem/Effects/MovementEffect.cs
+++ Assets/Scripts/CutsceneSystem/Effects/MovementEffect.cs
@@ -10,7 +10,21 @@ public class MovementEffect : Effect
     [SerializeField]
     private Vector3 target;
     [SerializeField]
-    private float speed;
+    private float duration;
+
+    //TODO: Easing effects;
+
+    //Step = CurrentTime / TotalDuration
+    //EasedStep = Easing.EaseIn(Step, EasingType.Quadratic)
+    //CurrentPosition = Lerp(OldPosition, NewPosition, EasedStep)
+    // every update, provided a delta time for that frame
+    //    sinceStarted += deltaTime;
+    //    float step = Math.Min(sinceStarted / duration, 1.0);
+    //    float easedStep = Easing.EaseIn(step, EasingType.Quadratic);
+    //    float easedValue = source + (dest - source) * easedStep;
+    //    // or...
+    //    float easedValue = MathHelper.Lerp(source, dest, easedStep);
+
 
     public override void Setup(CutscenePanel panel, GameObject gameObject)
     {
@@ -19,7 +33,11 @@ public class MovementEffect : Effect
 
     public override void Apply(CutscenePanel panel,GameObject gameObject)
     {
-        gameObject.transform.localPosition = Vector3.MoveTowards(gameObject.transform.localPosition, target, speed);
+        float time =+ Time.deltaTime;
+        float step = time / duration;
+        gameObject.transform.localPosition = Vector3.Lerp(gameObject.transform.localPosition, target, step);
+
+    //    gameObject.transform.localPosition = Vector3.MoveTowards(gameObject.transform.localPosition, target, speed);
     }
 
 }
